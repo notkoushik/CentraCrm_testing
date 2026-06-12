cf1587a78c0b888b4066c00826fd370a1cde35ca
notkoushik <nanikoushik96@gmail.com>
2026-06-12 16:25:08 +0530

    Migrate deprecated lucide_icons to lucide_icons_flutter drop-in package

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import '../state/crm_state.dart';
 
 class DashboardScreen extends ConsumerWidget {
